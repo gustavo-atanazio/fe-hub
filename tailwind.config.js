2d12182fc00f79622cf34cc137a4932b211699a8
Gustavo Atanazio <gus.atanazio@gmail.com>
2024-09-14 14:45:03 -0300

    chore: adicionando fonte do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,7 +54,10 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
-  		}
+  		},
+      fontFamily: {
+        sans: ['var(--roboto)']
+      }
   	}
   },
   plugins: [import('tailwindcss-animate')]
