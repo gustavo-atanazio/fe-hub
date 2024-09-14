ccc165d5deeac708b5a7507a3ad1d6a10bf75e2b
Gustavo Atanazio <gus.atanazio@gmail.com>
2024-09-14 12:10:32 -0300

    chore: corrigindo configurações do ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,30 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,jsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-
   {
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
     languageOptions: {
-      globals: { ...globals.browser },
+      globals: globals.browser,
       ecmaVersion: 'latest',
-      sourceType: 'module'
+      sourceType: 'module',
+      parser: typescriptParser
     },
-
+    plugins: {
+      '@typescript-eslint': typescriptPlugin
+    },
     rules: {
       indent: ['error', 2, { SwitchCase: 1 }],
       'linebreak-style': ['error', 'windows'],
       quotes: ['error', 'single'],
       semi: ['error', 'always'],
       'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off'
+      'react/prop-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/ban-ts-comment': 'warn'
     },
-    
     settings: {
       react: { version: 'detect' }
     }
