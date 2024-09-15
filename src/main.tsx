2da568a3e66b4dc8db4f1b20933fb9a76f465828
Gustavo Atanazio <gus.atanazio@gmail.com>
2024-09-15 16:18:51 -0300

    feat: dark mode (shadcn)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import { UserContextProvider } from './context/User';
+import { ThemeProvider } from './context/theme-provider';
 import Routes from './routes';
+
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <UserContextProvider>
-      <Routes/>
+      <ThemeProvider>
+        <Routes/>
+      </ThemeProvider>
     </UserContextProvider>
   </StrictMode>
 );
