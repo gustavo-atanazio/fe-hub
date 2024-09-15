121864533b1137f2ad31d3681628126e0944ecb0
Gustavo Atanazio <gus.atanazio@gmail.com>
2024-09-15 16:39:05 -0300

    style: dark mode/título

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,6 +6,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { ModeToggle } from '@/components/ui/mode-toggle';
 import { Separator } from '@/components/ui/separator';
 
 import Autoplay from 'embla-carousel-autoplay';
@@ -63,92 +64,102 @@ function Login() {
           <CarouselContent className='h-full'>
             <CarouselItem className='h-full'>
               <div className='h-full'>
-                <img src={car1} className='object-cover w-full h-full' alt='Carro 1'/>
+                <img src={car1} className='object-cover w-full h-full' alt='Carro 1' />
               </div>
             </CarouselItem>
 
             <CarouselItem className='h-full'>
               <div className='h-full'>
-                <img src={car2} className='object-cover w-full h-full' alt='Carro 2'/>
+                <img src={car2} className='object-cover w-full h-full' alt='Carro 2' />
               </div>
             </CarouselItem>
           </CarouselContent>
         </Carousel>
       </div>
 
-      <section className='w-full max-w-3xl h-full bg-background p-4 flex items-center justify-center'>
-        <Card className='w-full max-w-md'>
-          <CardHeader>
-            <CardTitle className='text-2xl font-bold tracking-tighter'>
-              Entre com sua conta
-            </CardTitle>
+      <section className='relative w-full max-w-3xl h-full bg-background p-8 md:p-4 flex flex-col items-center justify-center'>
+        <div className='absolute top-8 right-8'>
+          <ModeToggle/>
+        </div>
 
-            <CardDescription>
-              Utilize seu e-mail e senha ou entre com o Google
-            </CardDescription>
-          </CardHeader>
+        <div className='max-w-md flex flex-col gap-10'>
+          <h1 className='self-start font-bold text-6xl'>
+            FE Hub
+          </h1>
 
-          <CardContent>
-            <form onSubmit={handleSubmit}>
-              <div>
-                <Label htmlFor='email'>E-mail</Label>
-                <Input
-                  id='email'
-                  placeholder='exemplo@email.com'
-                  type='email'
-                  value={email}
-                  onChange={e => setEmailField(e.target.value)}
-                  required
-                />
-              </div>
+          <Card className='w-full'>
+            <CardHeader>
+              <CardTitle className='text-2xl font-bold tracking-tighter'>
+                Entre com sua conta
+              </CardTitle>
 
-              <div className='mt-4'>
-                <Label htmlFor='password'>Senha</Label>
-                <Input
-                  id='password'
-                  placeholder='Sua senha'
-                  type='password'
-                  value={password}
-                  onChange={e => setPasswordField(e.target.value)}
-                  required
-                />
-              </div>
+              <CardDescription>
+                Utilize seu e-mail e senha ou entre com o Google
+              </CardDescription>
+            </CardHeader>
 
-              <div className='mt-4'>
-                <Label htmlFor='username'>Nome de usuário</Label>
-                <Input
-                  id='username'
-                  placeholder='Seu nome'
-                  type='text'
-                  value={name}
-                  onChange={e => setNameField(e.target.value)}
-                  required
-                />
-              </div>
+            <CardContent>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <Label htmlFor='email'>E-mail</Label>
+                  <Input
+                    id='email'
+                    placeholder='exemplo@email.com'
+                    type='email'
+                    value={email}
+                    onChange={e => setEmailField(e.target.value)}
+                    required
+                  />
+                </div>
 
-              <Button className='mt-6 w-full' type='submit'>
-                Entrar
-              </Button>
+                <div className='mt-4'>
+                  <Label htmlFor='password'>Senha</Label>
+                  <Input
+                    id='password'
+                    placeholder='Sua senha'
+                    type='password'
+                    value={password}
+                    onChange={e => setPasswordField(e.target.value)}
+                    required
+                  />
+                </div>
 
-              <div className='flex items-center gap-6 mt-4'>
-                <Separator className='w-full shrink'/>
-                <span className='text-xs text-muted-foreground'>OU</span>
-                <Separator className='w-full shrink'/>
-              </div>
+                <div className='mt-4'>
+                  <Label htmlFor='username'>Nome de usuário</Label>
+                  <Input
+                    id='username'
+                    placeholder='Seu nome'
+                    type='text'
+                    value={name}
+                    onChange={e => setNameField(e.target.value)}
+                    required
+                  />
+                </div>
 
-              <Button variant='outline' className='mt-4 w-full'>
-                Entrar com o Google
-                <FcGoogle className='ml-2'/>
-              </Button>
-            </form>
-          </CardContent>
+                <Button className='mt-6 w-full' type='submit'>
+                  Entrar
+                </Button>
 
-          <CardFooter>
-            <p className='text-muted-foreground text-center text-sm'>
-              Ao entrar em nossa plataforma, você concorda com nossos Termos de Uso e Política de Privacidade.
-            </p>
-          </CardFooter>
-        </Card>
+                <div className='flex items-center gap-6 mt-4'>
+                  <Separator className='w-full shrink'/>
+                  <span className='text-xs text-muted-foreground'>OU</span>
+                  <Separator className='w-full shrink'/>
+                </div>
+
+                <Button variant='outline' className='mt-4 w-full'>
+                  Entrar com o Google
+                  <FcGoogle className='ml-2'/>
+                </Button>
+              </form>
+            </CardContent>
+
+            <CardFooter>
+              <p className='text-muted-foreground text-center text-sm'>
+                Ao entrar em nossa plataforma, você concorda com nossos Termos de Uso e Política de Privacidade.
+              </p>
+            </CardFooter>
+          </Card>
+        </div>
       </section>
     </main>
   );
