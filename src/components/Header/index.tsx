e428453a096279af9de3927dab613c544228fdb5
Gustavo Atanazio <gus.atanazio@gmail.com>
2024-09-21 16:20:51 -0300

    feat: componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from '../ui/mode-toggle';
+
+function Header() {
+  return (
+    <header className='hidden md:flex w-full justify-between items-center h-[60px] border-b p-4'>
+      <h1>FE Hub</h1>
+      
+      <ModeToggle/>
+    </header>
+  );
+}
+
+export default Header;
