f8631689c8cbbe4fb8b0c0fabbf066de1a62f54a
Gustavo Atanazio <gus.atanazio@gmail.com>
2024-09-21 16:51:09 -0300

    style: aumentando peso do texto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,9 @@ import { ModeToggle } from '../ui/mode-toggle';
 function Header() {
   return (
     <header className='hidden md:flex w-full justify-between items-center h-[60px] border-b p-4'>
-      <h1>FE Hub</h1>
+      <h1 className='font-bold'>
+        FE Hub
+      </h1>
       
       <ModeToggle/>
     </header>
