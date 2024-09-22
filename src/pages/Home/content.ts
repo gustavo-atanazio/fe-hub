f33ee3b072af510e0225adbd903b7cd0fa17bed4
Gustavo Atanazio <gus.atanazio@gmail.com>
2024-09-22 16:21:41 -0300

    feat: página Home

---
+++ src/pages/Home/content.ts
@@ -0,0 +1,59 @@
+import car from '@/assets/img/car_1.jpg';
+import car2 from '@/assets/img/car_2.jpg';
+
+const recents = [
+  {
+    img: car,
+    title: 'Calendário 2025 é anunciado este sábado',
+    text: 'A Fórmula E está prestes a revelar o tão aguardado calendário da temporada 2025. Com a promessa de novas cidades, circuitos desafiadores e mudanças estratégicas, a expectativa é de um dos campeonatos mais emocionantes até agora.'
+  },
+  {
+    img: car2,
+    title: 'Nova equipe confirmada para a temporada 2025',
+    text: 'A temporada de 2025 da Fórmula E trará novidades com a inclusão de uma nova equipe de fabricantes. Essa adição promete mexer com a dinâmica do campeonato e acirrar ainda mais a disputa pelo título.'
+  },
+  {
+    img: car,
+    title: 'Atualização dos carros Gen3: O que esperar?',
+    text: 'Os carros Gen3 da Fórmula E estão recebendo importantes atualizações, com foco em eficiência energética e desempenho. As novas especificações prometem elevar ainda mais o nível das corridas em 2025.'
+  },
+  {
+    img: car2,
+    title: 'Vergne fala sobre suas expectativas para 2025',
+    text: 'Jean-Éric Vergne, bicampeão da Fórmula E, compartilhou suas expectativas para a nova temporada. Com uma equipe reformulada e um carro mais competitivo, o piloto francês acredita que 2025 será um ano de grandes conquistas.'
+  },
+  {
+    img: car,
+    title: 'Fórmula E anuncia expansão para novos mercados',
+    text: 'A Fórmula E planeja expandir suas corridas para novos países, incluindo locais inéditos na América do Sul e Ásia. A expectativa é que a temporada de 2025 marque o início dessa nova fase de crescimento global da categoria.'
+  }
+];
+
+const topics = [
+  {
+    title: 'F1 x FE: Conheça as Diferenças',
+    text: 'Enquanto a Fórmula 1 se destaca pelo glamour e tradição, a Fórmula E tem inovado com sua pegada sustentável e foco em mobilidade elétrica. Entenda as principais diferenças entre as duas categorias e o que torna a FE única.'
+  },
+  {
+    title: 'Tecnologia de Baterias: O Futuro das Corridas Elétricas',
+    text: 'As baterias são o coração da Fórmula E, e a cada temporada, vemos avanços significativos. Como as inovações no armazenamento de energia estão moldando o futuro das corridas e impactando diretamente a indústria automotiva?'
+  },
+  {
+    title: 'Impacto Ambiental: Como a FE Está Redefinindo o Automobilismo',
+    text: 'A Fórmula E se comprometeu a ser neutra em carbono desde a sua criação. Saiba como a categoria está redefinindo o automobilismo ao adotar práticas sustentáveis e apoiar a transição para um futuro elétrico.'
+  },
+  {
+    title: 'Circuitos Urbanos: A Revolução no Design de Pistas',
+    text: 'Diferente das pistas tradicionais, a Fórmula E corre em circuitos urbanos no coração das cidades. Descubra como essa escolha impacta o design das corridas e aproxima a categoria de um público mais amplo.'
+  },
+  {
+    title: 'Pilotos que Brilham: As Estrelas da Fórmula E',
+    text: 'A Fórmula E tem atraído alguns dos maiores talentos do automobilismo. Conheça os pilotos que estão fazendo história na categoria e desafiando os limites da performance em carros elétricos.'
+  },
+  {
+    title: 'O Papel da FIA na Evolução da Fórmula E',
+    text: 'A FIA desempenha um papel crucial na regulação e desenvolvimento da Fórmula E. Entenda como a federação está ajudando a moldar o futuro das corridas elétricas e garantindo a competitividade da categoria.'
+  }
+];
+
+export { recents, topics };
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,56 @@
+import { Card, CardContent } from '@/components/ui/card';
+import { recents, topics } from './content';
+
+function Home() {
+  return (
+    <>
+      <div className='flex flex-col gap-2 md:col-span-2 lg:col-span-3'>
+        <h2 className='text-2xl font-medium'>
+          Recentes
+        </h2>
+
+        <div className='flex flex-col gap-4 md:max-h-full'>
+          {recents.map(({ img, title, text }, index) => (
+            <Card key={index}>
+              <CardContent className='flex gap-4 p-2 max-h-32'>
+                <div className='max-w-28 max-h-28 md:max-w-40 md:max-h-full'>
+                  <img src={img} className='rounded-lg h-full object-cover'/>
+                </div>
+
+                <div className='flex flex-col gap-1'>
+                  <h5 className='text-base font-semibold'>
+                    {title}
+                  </h5>
+                  <p className='text-xs line-clamp-3 md:line-clamp-4'>
+                    {text}
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+
+      <div className='flex flex-col gap-2'>
+        <h2 className='text-2xl font-medium text-center'>
+          Assuntos
+        </h2>
+
+        <Card className='h-full'>
+          <CardContent className='flex flex-col gap-2 p-4'>
+            {topics.map(({ title, text }, index) => (
+              <Card key={index}>
+                <CardContent className='flex flex-col gap-1 p-2'>
+                  <h4 className='text-base font-medium line-clamp-2'>{title}</h4>
+                  <p className='text-[12px] line-clamp-4'>{text}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+}
+
+export default Home;
